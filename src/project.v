6b038b8b0cd53f6d152698e9f4d0fa90f20618b0
Robert Vitaliani <robert.vitaliani@gmail.com>
2026-05-28 15:36:08 +0200

    Dodan RISC-V diplomski projekt

---
+++ src/project.v
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2024 Your Name
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+`default_nettype none
+
+module tt_um_asic_cpu (
+    input  wire [7:0] ui_in,    // Dedicated inputs
+    output wire [7:0] uo_out,   // Dedicated outputs
+    input  wire [7:0] uio_in,   // IOs: Input path
+    output wire [7:0] uio_out,  // IOs: Output path
+    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
+    input  wire       ena,      // always 1 when the design is powered, so you can ignore it
+    input  wire       clk,      // clock
+    input  wire       rst_n     // reset_n - low to reset
+);
+
+  wire rst = ~rst_n;
+  CPU_Core core(.clk(clk), 
+                .rst(rst), 
+                .instruction({ui_in, uio_in}),
+                .CPU_out(uo_out)
+  );
+
+  assign uio_oe  = 8'b00000000;
+  
+  
+  // All output pins must be assigned. If not used, assign to 0.
+  //assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
+  assign uio_out = 0;
+  
+
+  // List all unused inputs to prevent warnings
+  wire _unused = &{ena, 1'b0};
+  
+endmodule
